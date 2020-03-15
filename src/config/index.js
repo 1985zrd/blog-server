f0a2229f6764fc941a2d95c1dfba9c7ea654d227
张忍德 <zhangrende@zhangrendedeMacBook-Pro.local>
2020-03-15 22:20:50 +0800

    del

--- src/config/index.js
+++ src/config/index.js
@@ -1,15 +1,15 @@
 const { getIPAddress } = require('../utils')
 
-// let serverURL = `http://${getIPAddress()}:9000`
-let serverURL = ''
+let serverURL = `http://${getIPAddress()}:9000`
+// let serverURL = ''
 
 module.exports = {
   app: {
     keys: ['rd_1985@163.com']
   },
   db: {
-    // url: 'mongodb://1985zrd:Zrd-database@120.27.8.21:27017/blog',
-    url: 'mongodb://1985zrd:Zrd-database@172.31.141.72:27017/blog',
+    url: 'mongodb://1985zrd:Zrd-database@120.27.8.21:27017/blog',
+    // url: 'mongodb://1985zrd:Zrd-database@172.31.141.72:27017/blog',
     options: {
       useNewUrlParser: true,
       useUnifiedTopology: true
